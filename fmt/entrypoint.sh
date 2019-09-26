2cbdce5b3ce094049bb6a1d7d3137149051eb914
kenzo.tanaka <kenzo.tanaka@medpeer.co.jp>
2019-09-26 15:42:35 +0900

    delete -recursive option

--- fmt/entrypoint.sh
+++ fmt/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 cd "${TF_ACTION_WORKING_DIR:-.}"
 
 set +e
-OUTPUT=$(sh -c "terraform fmt -no-color -check -list -recursive $*" 2>&1)
+OUTPUT=$(sh -c "terraform fmt -no-color -check -list $*" 2>&1)
 SUCCESS=$?
 echo "$OUTPUT"
 set -e
